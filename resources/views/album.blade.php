3b046057d9cce0d4bf8ad228afd7dd3539915a4a
TahaMedhous <tahamedhous@gmail.com>
2023-07-13 04:45:04 -0700

    fonts

--- resources/views/album.blade.php
+++ resources/views/album.blade.php
@@ -10,6 +10,9 @@
     </title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script type="text/javascript" src="{{asset('js/app.js') }}" defer></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/app.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
